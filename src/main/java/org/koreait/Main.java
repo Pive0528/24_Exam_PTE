b6f22376b276ceffd81914bf9d82d54a74bb961d
Pive0528 <pive0528@gmail.com>
2024-06-27 14:38:14 +0900

    ã3ë²번 문제

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,22 +1,16 @@
 package org.koreait;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();        // 5
-
-        for (int i = num1; i > 0; i--) {       // 5...4...3...2...1
-            for (int j = num1-i; j > 0; j--) {    // 0...1...2...3...4
-                System.out.print(" ");
-            }
-            for (int k = i; k > 0; k--) {    // 5...4...3...2...1
-                System.out.print(k+" ");
-            }
-
-            System.out.println();
+        int num1 = 0;   // 처음 시작 값 0
+        int num2 = 1;   // 보조용? 두번째 값 1
+        int num3 = 0;   // 출력되는 값, 합쳐지는 최종값
+        for (int i = num1; i < 10; i++) {
+            System.out.print(num3 + " ");
+            num1 = num2;    // 0->1...
+            num2 = num3;    // 1->0...
+            num3 = num1 + num2; // 합쳐지는 값?
         }
     }
 }
+
