cd39fd3272d8b37aa29fcf337ad79dc818bc7ead
Pive0528 <pive0528@gmail.com>
2024-06-27 14:10:46 +0900

    2번 문제

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc  = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();        // 5
 
-        for (int i = 1; i<=num2; i++) {
-            for (int j = 1; j<=num1; j++) {
-                System.out.printf("*");
+        for (int i = num1; i > 0; i--) {       // 5...4...3...2...1
+            for (int j = num1-i; j > 0; j--) {    // 0...1...2...3...4
+                System.out.print(" ");
             }
+            for (int k = i; k > 0; k--) {    // 5...4...3...2...1
+                System.out.print(k+" ");
+            }
+
             System.out.println();
         }
     }
